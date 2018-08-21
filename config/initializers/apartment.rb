fbd35eff0eaee0e6de56aaadda50dc942ab9fab1
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-22 09:43:57 +1000

    add user

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -6,6 +6,8 @@ Apartment.configure do |config|
   config.excluded_models = ["User", "Customer"]
   config.tenant_names = lambda { User.pluck :site_name }
 
+  
 end
 
 Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
