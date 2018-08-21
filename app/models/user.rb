2333cfabba4da430e09f253d14107a22deb4a7f9
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-22 09:32:17 +1000

    add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  validates :site_name, presence: true,
+                            uniqueness: { case_sensitive: false},
+                            format: { with: /\A[\w\-]+\Z/i, message: 'contains invalid characters' },
+                            exclusion: {in: RESTRICTED_SUBDOMAIN, message: 'restricted'}
 end
