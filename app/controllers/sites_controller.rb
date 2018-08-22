096e9825bc4c57319909fa2597d29b687c1f8ba4
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-22 14:36:16 +1000

    better apartments

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -194,8 +194,8 @@ class SitesController < ApplicationController
     Service.create(title: 'Timber Pests', trade_id: 10)
     Service.create(title: 'General Pests', trade_id: 10)
 
-    Apartment::Tenant.switch('tenant_name') do
-      current_user.site_name
+    Apartment::Tenant.switch(current_user.site_name) do
+      
     end
     redirect_to new_site_url(subdomain: "#{current_user.site_name}.2min")
   end
