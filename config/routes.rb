5843572fd8ab4df3b09eec25480fba6bc2678621
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-22 14:28:31 +1000

    better apartments

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
-  get 'charge/payment_page'
-  post 'charge/create_charge'
-  get 'charge/create_charge'
-  get 'charge/thank_you'
-  get 'charge/already_have_domain'
-  get 'charge/need_domain'
-  resources :messages
-  resources :reviews
-  resources :site_services
-  resources :site_trades
-  resources :accreditation_and_licences
-  resources :personal_profiles
-  resources :portfolios
-  resources :intros
-  resources :colors
-  resources :service_locations
-  resources :additional_services
-  resources :sites
+
+  constraints subdomain: /^(?!www)(\w+)/ do
+    get 'charge/payment_page'
+    post 'charge/create_charge'
+    get 'charge/create_charge'
+    get 'charge/thank_you'
+    get 'charge/already_have_domain'
+    get 'charge/need_domain'
+    resources :messages
+    resources :reviews
+    resources :site_services
+    resources :site_trades
+    resources :accreditation_and_licences
+    resources :personal_profiles
+    resources :portfolios
+    resources :intros
+    resources :colors
+    resources :service_locations
+    resources :additional_services
+    resources :sites
+  end
+
+  
   get 'tenant' => 'sites#tenant'
   resources :services
   resources :trades
