b6dc0e5b7b616d7be5e0a4c34d258e6e440971aa
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-26 11:22:54 +1000

    add apartment gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,12 @@ class ApplicationController < ActionController::Base
 	require 'securerandom'
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-	before_filter :add_www_subdomain
-
-	  
-
 	  protected
 
 	  def configure_permitted_parameters
 	    devise_parameter_sanitizer.permit(:sign_up, keys: [:subdomain])
 	  end
 
-	  private
-	  def add_www_subdomain
-	    unless /^www/.match(request.host)
-	      redirect_to("#{request.protocol}x.com#{request.request_uri}",
-	                  :status => 301)
-	    end
-	  end
+	 
 	
 end
