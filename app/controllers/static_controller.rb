d4bcad77dbe076171e7f847c9347ca9afee0afaa
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-26 12:35:46 +1000

    add subdomain

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,7 @@ class StaticController < ApplicationController
   end
 
   def session_placeholder
+  	reset_session
   	session[:company] = params[:company_name]
   	
   	redirect_to new_user_registration_path
