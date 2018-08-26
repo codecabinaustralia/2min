0ee98c5c90861c7a41ff3f9ddbb849bea384f5ea
Josh Edgar <josh@blackhatdigital.com.au>
2018-08-26 11:28:37 +1000

    add apartment gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 	require 'securerandom'
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+
 	  protected
 
 	  def configure_permitted_parameters
