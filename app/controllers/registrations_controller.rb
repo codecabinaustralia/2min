451475003bcb052217f6b505dce6abff11396ae9
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-03 13:06:52 +1000

    add new user update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
 
   def after_sign_up_path_for(resource)
     tenant_url(:subdomain => resource.subdomain)
