9bdf8076d69619d69aebb04fb48b4deee7c0bc6b
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-03 12:55:50 +1000

    add db

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     tenant_url(:subdomain => resource.subdomain)
   end
+
+  def after_update_path_for(resource)
+	  root_url(:subdomain => resource.subdomain)
+	end
+
 end
