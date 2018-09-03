10230043ccff58184de10bb686a5cbd8d35c452d
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-03 13:45:05 +1000

    add new password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
 def update
-	sign_in(current_user, :bypass => true)
+  if current_user.update_with_password(params[:user])
+    sign_in(current_user, :bypass => true)
+    redirect_to root_url(:subdomain => resource.subdomain)
+  else
+    render :action => :show
+  end
 end
 
   protected
