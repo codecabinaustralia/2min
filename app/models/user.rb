1ff5bd305676be9e8f4f80ae12111c3c6484f780
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-06 07:20:29 +1000

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   after_create :create_tenant
-  validates :email, uniqueness: { message: 'Sorry this email has already been taken, try the forgotten password when you login in.' }
   validates :email, presence:{ message: 'Silly! You must add an email.' }
   validates :subdomain, uniqueness: { message: 'Unfortunately someone already has your website name, try to name it slightly different' }
   validates :subdomain, presence:{ message: 'Oh man, you need to enter a company name.' }
-  validates :subdomain, format: { with: /\A[a-zA-Z]+\z/,
-    message: "Sorry you're only allowed to use letters" }
 
   private
 
@@ -21,3 +18,4 @@ class User < ApplicationRecord
   end
 
 end
+
