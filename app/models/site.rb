def9b029a8816ca8182237c50f513049379dc3ed
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-06 07:54:26 +1000

    add validation

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,8 @@ class Site < ApplicationRecord
   belongs_to :user
   has_many :trades
   has_one_attached :featured_image
+
+  validates :phone, uniqueness: { message: "We'll do our best to send you leads but they'll need a number to call?" }
+  validates :home_town, presence:{ message: 'We need to tell people your home city, you could also just put ie: Australia' }
+
 end
