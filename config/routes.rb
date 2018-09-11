7327feeda2a97d76371259403cd4da9a752add53
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-11 17:29:54 +1000

    add deploy domain to restricted urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 class SubdomainContraint
 
   def self.matches?(request)
-    subdomains = %w{www admin }
+    subdomains = %w{www admin g407}
     request.subdomain.present? && !subdomains.include?(request.subdomain)
   end
 
