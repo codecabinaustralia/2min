904c023b9116e18c45b0c12eafb0939ee4c843a5
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-12 12:33:54 +1000

    add better session

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,7 +7,7 @@ class StaticController < ApplicationController
 
   def session_placeholder
   	reset_session
-  	session[:company] = params[:company_name]
+  	session[:company] = params[:company_name].downcase.gsub(/\s+/, "")
   	
   	redirect_to new_user_registration_path
   end
--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -105,5 +105,6 @@ end
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
 Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'admin', 'mdoz', 'public', 'mytest']
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
