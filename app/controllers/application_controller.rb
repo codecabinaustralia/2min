a6121e4579561d2300d6bb8f246a7065ebc93a45
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-16 14:40:33 +1000

    add domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 	  protected
 
 	  def configure_permitted_parameters
-	    devise_parameter_sanitizer.permit(:sign_up, keys: [:subdomain])
+	    devise_parameter_sanitizer.permit(:sign_up, keys: [:subdomain, :domain])
 	  end
 
 	 
