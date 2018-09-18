8f68a5a9d06980fc05f1cc45537f2387f6beeabb
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-18 11:00:30 +1000

    add domain

--- app/controllers/charge_controller.rb
+++ app/controllers/charge_controller.rb
@@ -47,7 +47,7 @@ class ChargeController < ApplicationController
     domain = @site.company_name + ".com.au"
     #Check domain
     whois = Whois::Client.new
-    record =  whois.lookup(domain)
+    record =  whois.lookup(domain.to_s)
     parser = record.parser
 
     @domain_available = parser.available?
