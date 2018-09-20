18f87fabd42acb1a44e0e1cd51d9708cf1959308
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-20 16:00:24 +1000

    add templates

--- app/models/template_custom.rb
+++ app/models/template_custom.rb
@@ -1,2 +1,11 @@
 class TemplateCustom < ApplicationRecord
+	has_one_attached :header_1_img
+	has_one_attached :content_6col_leftimg1
+	has_one_attached :content_6col_leftimg2
+	has_one_attached :content_6col_rightimg1
+	has_one_attached :content_6col_rightimg2
+	has_one_attached :services_1_hero
+	has_one_attached :about_1_img1
+	has_one_attached :about_1_img2
+
 end
