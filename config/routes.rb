15319269155a59292c4bbcd99f2efa7ba45688a4
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-20 19:57:47 +1000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,6 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  match '', to: 'sites#index', constraints: {subdomain: /.+/}
   root to: "static#home"
 end
