64420585c12daa5996450d37f5bce2ca68622031
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-20 20:03:19 +1000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,8 @@ Rails.application.routes.draw do
       get 'charge/create_charge'
       get 'charge/thank_you'
       get 'charge/building'
+
+      root to: "sites#index"
       
     end
 
@@ -58,6 +60,5 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'sites#index', constraints: {subdomain: /.+/}
   root to: "static#home"
 end
