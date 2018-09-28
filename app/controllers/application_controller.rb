8f5ed422b02a5bfa6234bd119d7ba295b2e56c60
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 07:26:55 +1000

    add verification files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
 	require 'securerandom'
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	#before_action :check_subdomain
-	
+	before_filter :redirect_subdomain
+
+	def redirect_subdomain
+	  if request.host == '2min.co'
+	    redirect_to 'http://www.2min.co' + request.fullpath, :status => 301
+	  end
+	end
+
 	def check_subdomain
       @host = request.host
       @host = @host.sub(/^www./,'')
