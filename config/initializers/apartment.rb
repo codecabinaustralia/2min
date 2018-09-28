7617ca7d793c13a99a14a79e87da987d3da74450
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-28 14:12:17 +1000

    move bin elevator pitch

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -106,10 +106,8 @@ end
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
 Rails.application.config.middleware.use Apartment::Elevators::Subdomain, -> (request) { 
 
-  @host = request.host
-  @host = @host.sub(/^www./,'')
-  @subdomain = @host.split('.').first
-  @found_user = User.where(domain: @host).last
+
+  @found_user = User.where(domain: request.host).last
   if @found_user.present?
     Apartment::Tenant.switch!(@found_user.subdomain)
   end
