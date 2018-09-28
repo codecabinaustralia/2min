5e978cf2c4ecec19805c5589ee367a63870756a9
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 07:30:14 +1000

    add verification files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
 	require 'securerandom'
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	#before_action :check_subdomain
-	before_action :redirect_subdomain
+	#before_action :redirect_subdomain
 
 	def redirect_subdomain
 	  if request.host == '2min.co'
@@ -10,17 +9,6 @@ class ApplicationController < ActionController::Base
 	  end
 	end
 
-	def check_subdomain
-      @host = request.host
-      @host = @host.sub(/^www./,'')
-      @found_user = User.where(domain: @host).last
-      if @found_user.present?
-      	  Apartment::Tenant.switch!(@found_user.subdomain)
-          redirect_to sites_url(:subdomain => request.subdomain)
-      else
-      	redirect_to root_path
-      end   
-	end
 
 	protected
 		def after_sign_in_path_for(resource)
