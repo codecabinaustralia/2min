bf2c05459dbcd01da80ee191b568c1ef9949d4a4
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 09:07:04 +1000

    remove force ssl

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Powerhouse
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.use Rack::SslEnforcer, :only_hosts => 'www.2min.co', :strict => true, :hsts => { :expires => 0, :subdomains => false, :preload => false }
+    config.middleware.use Rack::SslEnforcer, :hsts => { :expires => 0, :subdomains => false, :preload => false }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
