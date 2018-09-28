3b482bba1df8bbe32db467bf312a79f4fdf219d3
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 07:56:25 +1000

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require_relative 'boot'
 require 'apartment/elevators/subdomain'
 require 'rails/all'
 require "active_storage"
+require 'rack/ssl-enforcer'
 
+ use Rack::SslEnforcer
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,6 +16,8 @@ module Powerhouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.middleware.use Rack::SslEnforcer, :only_hosts => 'www.2min.co', :strict => true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
