0aee33156d1448141aaf630f7842a080b6499766
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 09:03:17 +1000

    remove force ssl

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 require 'apartment/elevators/subdomain'
 require 'rails/all'
 require "active_storage"
+require 'rack/ssl-enforcer'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +15,8 @@ module Powerhouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.middleware.use Rack::SslEnforcer, :only_hosts => 'www.2min.co', :strict => true, :hsts => { :expires => 0, :subdomains => false, :preload => false }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
