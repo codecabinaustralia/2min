9a9c961f1fd5436dc70c034a0f601271ffc813c7
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 07:57:19 +1000

    force ssl

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 require "active_storage"
 require 'rack/ssl-enforcer'
 
- use Rack::SslEnforcer
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
