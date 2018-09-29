deae326d754a0a9ee59efc91ec906bfe811d3ac5
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-30 07:35:14 +1000

    add site new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
 	require 'securerandom'
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :redirect_subdomain
-	force_ssl if: :ssl_configured?
-
-	
 
 
 	protected
@@ -13,10 +10,6 @@ class ApplicationController < ActionController::Base
 		    redirect_to 'https://www.2min.co' + request.fullpath, :status => 301
 		  end
 		end
-
-	    def ssl_configured?
-	        request.host == 'www.2min.co'
-	      end
 
 		def after_sign_in_path_for(resource)
 		  sites_url(:subdomain => resource.subdomain)
--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,6 @@ module Powerhouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    config.middleware.insert_before ActionDispatch::Static, Rack::SSL, :exclude => proc { |env| env['HTTPS'] != 'on' }
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
