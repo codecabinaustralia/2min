7da0a10d231c72ef4bed562ebbaaa70c183ccf98
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-29 10:22:33 +1000

    add

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Powerhouse
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    #config.middleware.use Rack::SslEnforcer, :only => '/login'
+    config.middleware.use Rack::SslEnforcer, :only => [%r{^/users/*}]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
