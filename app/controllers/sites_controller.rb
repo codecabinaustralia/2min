8f8eb2092e3c5c2e5233ab5c336837bbe9b5bd81
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-30 22:47:19 +1000

    blue color

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -63,10 +63,10 @@ class SitesController < ApplicationController
 
     auth = {api_key: ENV['CAMPAIGN_MONITOR_API']}
     list_id = '628a07e4b814c79d71a4699262e5d642'
-    email, name, temporary_password = "#{current_user.email}", 'Alice'
+    email, name, temporary_password = "#{current_user.email}", 'Alice', 'Test123'
 
     begin
-      CreateSend::Subscriber.add(auth, list_id, email, name, [], false)
+      CreateSend::Subscriber.add(a(auth, list_id, email, name, temporary_password, false, false, false)
     rescue CreateSend::BadRequest => exception
       fail "could not add #{email} code=#{exception.data.Code}"
     end
