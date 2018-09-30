80f98dea8f099c2f5feb83ae6d050b3963e6e7bb
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-30 22:36:44 +1000

    blue color

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -63,7 +63,7 @@ class SitesController < ApplicationController
 
     auth = {api_key: ENV['CAMPAIGN_MONITOR_API']}
     list_id = '628a07e4b814c79d71a4699262e5d642'
-    email, name, temporary_password = current_user.email, 'Alice', 'Test123'
+    email, name, temporary_password = "#{current_user.email}", 'Alice', 'Test123'
     begin
       CreateSend::Subscriber.add(auth, list_id, email, name, temporary_password, [], false)
     rescue CreateSend::BadRequest => exception
