3c0188e01de651cd061e61c7960913f4dcd65d8c
Josh Edgar <josh@blackhatdigital.com.au>
2018-09-30 14:46:18 +1000

    blue color

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -31,7 +31,7 @@ class SitesController < ApplicationController
       company_name: session[:company],
       template_name: session[:template_name],
       home_town: "Sydney",
-      location_text: "We service all areas of Sydney and surroundingsuburbs. Call now and see if we can service your area."
+      location_text: "We service all areas of Sydney and surrounding suburbs. Call now and see if we can service your area.",
       phone: "0400 000 000",
       user_id: current_user.id
       )
